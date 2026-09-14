d521acfdc1f6fea15d519bd3ea449b1079dd2481
Luke Hopkins <lukehopkins@lukes-air.mynetworksettings.com>
2026-09-14 14:56:16 -0400

    updated resume after hackathon

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -87,7 +87,7 @@ export default function ContactPage() {
                   GitHub
                 </Link>
                 <Link
-                  href="/contact/lhresume2026.pdf"
+                  href="/contact/Hopkins_Luke_Resume.pdf"
                   target="_blank"
                   rel="noreferrer noopener"
                   className="rounded-full bg-white/10 px-4 py-2 text-sm backdrop-blur transition hover:bg-white/20"
